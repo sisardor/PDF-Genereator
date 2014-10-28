20117c0025a200c6136f178be52d6d030b137408
Sardor Isakov <sisardor@gmail.com>
2014-10-28 16:53:20 -0700

    Second pass header footer

--- src/Main.java
+++ src/Main.java
@@ -1,328 +1,169 @@
+
+import java.awt.Color;
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
 
-import com.itextpdf.text.Chunk;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
-import com.itextpdf.text.Font;
-import com.itextpdf.text.FontFactory;
-import com.itextpdf.text.List;
-import com.itextpdf.text.ListItem;
-import com.itextpdf.text.PageSize;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.Phrase;
-import com.itextpdf.text.Rectangle;
-import com.itextpdf.text.pdf.ColumnText;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfPageEventHelper;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.PdfStamper;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.blackice.persistence.model.Assessment;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.*;
+/*
+ *	this.source = Assessment.class
+ *		var json = [];
+	        var id = 0;
+	        for (var i = 0, len = this.source.length; i < len; i++) {
+	            if(id != this.source[i].directiveParagraph.directiveParagraphNum){
+	              	id = this.source[i].directiveParagraph.directiveParagraphNum
+	              	var assessments = this.get_assessments(i, this.source[i].directiveParagraph.directiveParagraphNum)
+	              	json.push({
+	                	regulation       		: this.source[i].directiveParagraph.directive.txtDirName,
+	                	directiveParagraphNum   : this.source[i].directiveParagraph.directiveParagraphNum , 
+	                	paragraphDescription 		: this.source[i].directiveParagraph.memDirParDetails, 
+	                	assessments          	: assessments
+	              	})
+	          	} 
+	        };
+	        return json
+ */
 public class Main {
 	//static Document document = new Document(PageSize.LETTER.rotate());
 	static Document document = new Document(PageSize.A4.rotate());
 	private static String text = "\tLorem Ipsum is simply dummy text of the printing and typesetting\t industry.\nLorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 	
-	static Font font8 = new Font(Font.FontFamily.HELVETICA, 		8);
-	static Font font9italic = new Font(Font.FontFamily.HELVETICA, 		9, Font.ITALIC);
-	static Font font8boldUnder = new Font(Font.FontFamily.HELVETICA, 		8, Font.BOLD | Font.UNDERLINE);
-	static Font font9bold = new Font(Font.FontFamily.HELVETICA, 		9);
+	static Font font8 = new Font(Font.HELVETICA, 		8);
+	static Font font9italic = new Font(Font.HELVETICA, 		9, Font.ITALIC);
+	static Font font8boldUnder = new Font(Font.HELVETICA, 		8, Font.BOLD | Font.UNDERLINE);
+	static Font font9bold = new Font(Font.HELVETICA, 		9);
 	
-	static Font font25 = new Font(Font.FontFamily.HELVETICA, 		25);
-	static Font font25bold = new Font(Font.FontFamily.HELVETICA, 	25, Font.BOLD);
-	static Font font18 = new Font(Font.FontFamily.HELVETICA, 		18);
-	static Font font18bold = new Font(Font.FontFamily.HELVETICA, 	18, Font.BOLD);
-	static Font font18italic = new Font(Font.FontFamily.HELVETICA, 18,	Font.ITALIC);
-	static Font font27 = new Font(Font.FontFamily.HELVETICA, 		27);
+	static Font font10 = new Font(Font.TIMES_ROMAN, 	10);
+	static Font font10bold = new Font(Font.TIMES_ROMAN, 	10, Font.BOLD);
+	
+	static Font font25 = new Font(Font.HELVETICA, 		25);
+	static Font font25bold = new Font(Font.HELVETICA, 	25, Font.BOLD);
+	static Font font18 = new Font(Font.HELVETICA, 		18);
+	static Font font18bold = new Font(Font.HELVETICA, 	18, Font.BOLD);
+	static Font font18italic = new Font(Font.HELVETICA, 18,	Font.ITALIC);
+	static Font font27 = new Font(Font.HELVETICA, 		27);
+	
+	static String headerLabel = "Bank Assessment Detail";
+	static String headerRegulation = "Regulation: %s";
+	static String headerAssDate = "Assessment Date: %s";
+	static String headerAssCriteria = "Assessment Criteria: %s";
 	
 	public static void main(String[] args) throws DocumentException, IOException {
-		//pdfParagarph();
-		pdfTable();
-		//pdfPhrase2();
 
-	}
-	
-	public void pdfPhrase() {
+		//pdfTable();
 
-
-
-        try {
-            PdfWriter.getInstance(document,
-                   new FileOutputStream("Phrase-l.pdf"));
-
-            document.open();
-            document.add(new Phrase("This is sentence 1. "));
-            document.add(new Phrase("This is sentence 2. "));
-            document.add(new Phrase("This is sentence 3. "));
-            document.add(new Phrase("This is sentence 4. "));
-            document.add(new Phrase("This is sentence 5. "));
-            document.add(new Phrase("This is sentence 6. "));
-            document.close();
-
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-	}
-	
-	public static void pdfPhrase2() {
-
-        try {
-            PdfWriter.getInstance(document,
-                new FileOutputStream("Phrase2.pdf"));
-
-            document.open();
-            Chunk chunk = new Chunk("This is a sentence ");
-
-            Phrase phrase = new Phrase(14);
-
-            phrase.add(chunk);
-            phrase.add(chunk);
-            phrase.add(chunk);
-            phrase.add(chunk);
-            phrase.add(chunk);
-            phrase.add(chunk);
-
-            phrase.add(chunk);
-            phrase.add(chunk);
-            phrase.add(chunk);
-            phrase.add(chunk);
-            phrase.add(chunk);
-            phrase.add(chunk);
-
-            phrase.add(chunk);
-            phrase.add(chunk);
-            phrase.add(chunk);
-            phrase.add(chunk);
-            phrase.add(chunk);
-            phrase.add(chunk);
-
-            document.add(phrase);
-            document.close();
-
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-	}
-	public static void pdfParagarph() {
-
-	    
-
-		try {
-            PdfWriter.getInstance(document,
-                new FileOutputStream("Paragraph2.pdf"));
-
-            document.open();
-            Paragraph paragraph1 = new Paragraph();
-            
-                Paragraph paragraph2 = new Paragraph(13);
-
-                paragraph2.setSpacingAfter(25);
-                paragraph2.setSpacingBefore(25);
-                paragraph2.setAlignment(Element.ALIGN_CENTER);
-                paragraph2.setIndentationLeft(50);
-                paragraph2.setIndentationRight(50);
-            
-
-            for(int i=0; i<10; i++){
-                Chunk chunk = new Chunk(
-                    "This is a sentence which is long " + i + ". ");
-                paragraph1.add(chunk);
-                paragraph2.add(chunk);
-            }
-
-            document.add(paragraph1);
-            document.add(paragraph2);
-            document.close();
-
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-	  }
-	
-	public static void pdfTable() throws DocumentException, IOException {
-		Font f1 = new Font();
-		f1.setSize(9);
-		 //HeaderFooter event = new HeaderFooter();
-		// step 1
-        //Document document = new Document(PageSize.A4, 36, 36, 54, 36);
-        // step 2
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PdfWriter.getInstance(document, baos);
-        // step 3
-        document.open();
-        // step 4
-
-        	//PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("xx.pdf"));
-//        	writer.setBoxSize("art", new Rectangle(50, 30, 25, 200));
-//        	writer.setPageEvent(event);
-//            document.setPageSize(PageSize.A4.rotate());
-//            document.setMargins(25, 20, 25, 20);
-//            document.setMarginMirroringTopBottom(true);
-            
-
-	        
-	        Paragraph paragraph = new Paragraph(15);
-	        paragraph.add(new Phrase(text));
-
-	        for (int i = 0; i < 20; i++) {
-				addDirParagraph(f1);
-				addAssessmentList(font8boldUnder);
-			}
+		
+		
+		ReportData assList = new ReportData();
+			assList.setDirectiveParagraphNum("1");
+			assList.setParagraphDescription("Description");
+			java.util.List<Assessment> ass = new ArrayList<Assessment>();
+			assList.setList(ass);
 			
+			
+		  
+		java.util.List<ReportData> list = new ArrayList<ReportData>();
+		ParagraphDetailsData data = new ParagraphDetailsData();
+			data.setData(list);
+			data.setHeaderLabel("Basel");
+			data.setHeaderRegulation("Basel");
+			data.setHeaderAssDate("2012-12-12");
+			data.setHeaderAssCriteria("<<All>>");
+		
+		ParagraphDetailsReport report = new ParagraphDetailsReport();
+		report.setReportData(data);
+		report.setDocument(document);
+		
+		OutputStream os = report.generateReport(new FileOutputStream("NewFile.pdf"));
+		os.flush();
 
-            document.close();
-            
-         // SECOND PASS, ADD THE HEADER
-            
-            // Create a reader
-            PdfReader reader = new PdfReader(baos.toByteArray());
-            // Create a stamper
-            PdfStamper stamper
-                = new PdfStamper(reader, new FileOutputStream("out.pdf"));
-            // Loop over the pages and add a header to each page
-            int n = reader.getNumberOfPages();
-            Rectangle mediabox = reader.getPageSize(1);
-            for (int i = 1; i <= n; i++) {
-            	System.out.println("page " +mediabox.getTop() + "," + mediabox.getRight());
-                getHeaderTable(i, n).writeSelectedRows(
-                        0, -1, 35, 27, stamper.getOverContent(i));
-                getHeaderTable(i, n).writeSelectedRows(
-                        0, -1, 35, 595-10, stamper.getOverContent(i));
-            }
-            // Close the stamper
-            stamper.close();
-            reader.close();
+//        // SECOND PASS, ADD THE HEADER
+//		OutputStream os = new FileOutputStream("out-to-file.pdf");
+//        // Create a reader
+//        PdfReader reader = new PdfReader(baos.toByteArray());
+//        // Create a stamper
+//        PdfStamper stamper = new PdfStamper(reader, os);
+//        // Loop over the pages and add a header to each page
+//        int n = reader.getNumberOfPages();
+//        Rectangle mediabox = reader.getPageSizeWithRotation(1);
+//        int pdfHeight = (int) mediabox.getHeight();
+//        int pdfWidth = (int) mediabox.getWidth();
+//        System.out.println("page " +pdfHeight + " x " + pdfWidth);
+//        for (int i = 1; i <= n; i++) {
+//        	getHeaderTable(i, n).writeSelectedRows(
+//                    0, -1, 35, pdfHeight-10, stamper.getOverContent(i));
+//        	
+//            getFooterTable(i, n).writeSelectedRows(
+//                    0, -1, 35, 27, stamper.getOverContent(i));
+//        }
+//        // Close the stamper
+//        stamper.close();
+//        reader.close();
+        
+
+        
+        // We write the PDF bytes to the OutputStream
+        
+//        baos.writeTo(os);
+//        os.flush();
+		
 
 	}
-	public static void addDirParagraph(Font f1) throws DocumentException {
-        PdfPTable table = new PdfPTable(2); // 3 columns.
-        table.setSpacingAfter(5);
-        PdfPCell parNumCell = new PdfPCell(new Paragraph("121", f1));
-        parNumCell.setBorderWidthBottom(0f);
-        parNumCell.setBorderWidthLeft(0f);
-        parNumCell.setBorderWidthTop(0f);
-        parNumCell.setPadding(3);
-        
-        Phrase memPargraphDetails = new Phrase();
-        memPargraphDetails.setLeading(25);
-        memPargraphDetails.setFont(f1);
-        memPargraphDetails.add(text);
-        
-        PdfPCell parDetailCell = new PdfPCell(memPargraphDetails);
-        parDetailCell.setPadding(3);
-        parDetailCell.setPaddingTop(1);
-        parDetailCell.setPaddingBottom(5);
-        parDetailCell.setLeading(10, 0);
-        
-        table.addCell(parNumCell);
-        table.addCell(parDetailCell);
-        table.setWidthPercentage(100);
-        table.setWidths(new int[]{30,400});
-        
-		document.add(table);
-		
-	}
-	public static void addAssessmentList(Font f1) throws DocumentException {
-		PdfPTable table = new PdfPTable(8);
-		table.getDefaultCell().setPaddingBottom(3);
-		table.getDefaultCell().setBorder(Rectangle.NO_BORDER); 
-		float[] columnWidths = {20f, 12f, 15f, 28f, 30f, 16f, 45f,45f};
 
-		table.setWidths(columnWidths);
-		
-		//table.getDefaultCell().setBorder(Rectangle.BOTTOM);
 
-		table.addCell(new Phrase("Bank", f1));
-		table.addCell(new Paragraph("WS", f1));
-		table.addCell(new Paragraph("1-Gap", f1));
-		table.addCell(new Paragraph("2-Project Status", f1));
-		table.addCell(new Paragraph("3-Compliance", f1));
-		table.addCell(new Paragraph("Target Date", f1));
-		table.addCell(new Paragraph("Measure of Success", f1));
-		table.addCell(new Paragraph("Comments", f1));
-		
-		table.addCell(new Phrase("Bank China America",font8));
-		table.addCell(new Paragraph("WS",font8));
-		table.addCell(new Paragraph("Major",font8));
-		table.addCell(new Paragraph("2-Peoject Status",font8));
-		table.addCell(new Paragraph("3-Compliance",font8));
-		table.addCell(new Paragraph("2014-23-12",font8));
-		
-		List unorderedList = new List(List.UNORDERED);
-        unorderedList.add(new ListItem("Item 1 Comment rinting and typesetting Lorem Ipsum is simply",font8));
-        unorderedList.add(new ListItem("Item 2 smg and typesetting Lorem Ipsum is simply",font8));
-        unorderedList.add(new ListItem("Item 3 Comment  Comment rinting and typesetting Lorem Ipsum is simplypesetting",font8));
-        
-        Phrase phrase = new Phrase();
-        phrase.add(unorderedList);
-        
-        // We add this phrase to a cell
-        PdfPCell phraseCell = new PdfPCell();
-        phraseCell.setBorder(Rectangle.NO_BORDER);
-        phraseCell.addElement(phrase);
-        phraseCell.setPaddingTop(-3);
-        
-		table.addCell(phraseCell );
-		table.addCell(new Paragraph("Comment rinting and typesetting Lorem Ipsum is simply dummy text ting and typesetting",font8));
-		
-		table.addCell(new Phrase("New West China America",font8));
-		table.addCell(new Paragraph("PMO",font8));
-		table.addCell(new Paragraph("Major",font8));
-		table.addCell(new Paragraph("Completed",font8));
-		table.addCell(new Paragraph("Substantial Compliance",font8));
-		table.addCell(new Paragraph("2014-23-12",font8));
-		table.addCell(new Paragraph("Lorem Ipsums simply dummy text of the printing and typesetting",font8));
-		table.addCell(new Paragraph("Commit ting and typesetting",font8));
-		
-		table.setWidthPercentage(100);
-		table.setSpacingAfter(18);
-		document.add(table);
-		
-	}
-	/**
-     * Create a header table with page X of Y
-     * @param x the page number
-     * @param y the total number of pages
-     * @return a table that can be used as header
-	 * @throws DocumentException 
-     */
     public static PdfPTable getHeaderTable(int x, int y) throws DocumentException {
+    	//Color color = new Color(50,50,50);
+    	
+        PdfPTable table = new PdfPTable(4);
+        table.setTotalWidth(775 );
+        table.getDefaultCell().setBorder(Rectangle.NO_BORDER);
+        table.getDefaultCell().setBorder(Rectangle.BOTTOM);
+        table.getDefaultCell().setBorderColor(Color.GRAY);
+        table.getDefaultCell().setBorderWidth(1.5f);
+        //table.getDefaultCell().setPaddingBottom(-3);
+
+        float[] columnWidths = new float[] {10f, 10f, 10f, 10f};
+        table.setWidths(columnWidths);
+        
+        table.getDefaultCell().setFixedHeight(20);
+
+        table.addCell(new Paragraph(headerLabel, font10bold));
+        	//table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_RIGHT);
+
+        table.addCell(new Paragraph(String.format(headerRegulation, "Basel II"), font10));
+        table.addCell(new Paragraph(String.format(headerAssDate, "2012-Oct-15"), font10));
+        table.addCell(new Paragraph(String.format(headerAssCriteria, "<<All>>"), font10));
+        return table;
+    }
+    
+    public static PdfPTable getFooterTable(int x, int y) throws DocumentException {
     	Font font = FontFactory.getFont("Times-Roman");
     	font.setSize(9);
     	font.setStyle(Font.ITALIC);
     	
         PdfPTable table = new PdfPTable(2);
         table.setTotalWidth(775 );
-        //table.setWidthPercentage(100);
+        table.getDefaultCell().setBorder(Rectangle.NO_BORDER);
+        table.getDefaultCell().setBorder(Rectangle.TOP);
+        //table.getDefaultCell().setBorderColor(Color.GRAY);
         table.setWidthPercentage(100);
         table.setWidths(new int[]{700,75});
         table.setLockedWidth(true);
         table.getDefaultCell().setFixedHeight(20);
 
-        PdfPCell cell = new PdfPCell(new Paragraph("Printed: Tuesday, Oct 27, 2014 10:52 PM", font));
-        cell.setBorder(Rectangle.NO_BORDER); 
-        cell.setBorder(Rectangle.TOP);
-        
-        table.addCell(cell);
+
+        table.addCell(new Paragraph("Printed: Tuesday, Oct 27, 2014 10:52 PM", font));
         table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_RIGHT);
-        PdfPCell cell2 = new PdfPCell(new Paragraph(String.format("Page %d of %d", x, y), font));
-        cell2.setBorder(Rectangle.NO_BORDER);
-        cell2.setBorder(Rectangle.TOP  );
-        
-        table.addCell(cell2);
+
+        table.addCell(new Paragraph(String.format("Page %d of %d", x, y), font));
         return table;
     }
+    
 
     
     
